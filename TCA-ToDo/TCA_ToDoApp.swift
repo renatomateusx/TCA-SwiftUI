4760cf61ef7e28dbd1358fdc62ef630397459938
renatomateusx <renatomateusx@gmail.com>
2022-07-12 10:41:36 -0300

    Initial Commit

---
+++ TCA-ToDo/TCA_ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_ToDoApp.swift
+//  TCA-ToDo
+//
+//  Created by Renato Mateus on 12/07/22.
+//
+
+import SwiftUI
+
+@main
+struct TCA_ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
